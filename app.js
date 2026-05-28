b3740455f28643a31e8942cf7b94c5fe32a02631
muslim code <essafi.idriss2007@gmail.com>
2026-05-28 11:06:38 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const nextButton = document.querySelector('.next-btn');
+const video = document.querySelector('.hero-video')
+
+const movieList = ['videos/hero-1.mp4','videos/hero-2.mp4','videos/hero-3.mp4','videos/hero-4.mp4'];
+
+let index = 0;
+nextButton.addEventListener('click',function(){
+
+      
+    index = (index + 1) % movieList.length;
+    video.src = movieList[index];
+    video.play()
+})
